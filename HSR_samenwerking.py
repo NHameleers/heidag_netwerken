c168d8a5486dcb10210aae73e261a8529280fc8d
Hameleers <Niels.Hameleers@unimaas.nl>
2022-05-30 16:57:44 +0200

    Add colors and shape

--- HSR_samenwerking.py
+++ HSR_samenwerking.py
@@ -1,5 +1,6 @@
 from venv import create
 
+import random
 import pandas as pd
 import networkx as nx
 from pyvis.network import Network
@@ -8,12 +9,41 @@ import streamlit as st
 import streamlit.components.v1 as components
 st.set_page_config(layout='wide')
 
+random.seed(10)
+
 VASTE_STAF_NAMEN = gp.get_vaste_staf_namelist('Data/HSR Vaste staf 01-05-2022.XLSX')
+
+
+
+
+
+
+
+with st.sidebar:
+    'Onderzoek:'
+    onderzoek_kleur = st.selectbox(label='Kleur de nodes naar...:',
+    options=['Geen indeling', 'Onderzoekslijn', 'Academische Werkplaats', 'Research Unit'])
+
+    'Onderwijs'
+    onderwijs_kleur = st.selectbox(label='Kleur de nodes naar...:',
+    options=['Geen indeling', 'Onderzoekslijn', 'Academische Werkplaats', 'Research Unit', 'GW'])
+
+
+
+
+
+
+
+
 
 
 '# HSR Samenwerking'
 
-G = gp.create_graph('Data/2020_2021_HSR_publications.json')
+G = gp.create_onderzoek_graph('Data/2020_2021_HSR_publications.json')
+
+if onderzoek_kleur is not 'Geen indeling':
+    gp.kleur_nodes_volgens_kolom(G, onderzoek_kleur)
+
 
 # Initiate PyVis network object
 onderzoek_net = Network(height='600px', width='1000px', bgcolor='white', font_color='black')
@@ -39,10 +69,42 @@ except:
 components.html(HtmlFile.read(), height=600, width=1000)
 
 
-namen_in_netwerk = [G.nodes[id]['label'] for id in G.nodes]
-niet_in_netwerk = [naam for naam in VASTE_STAF_NAMEN if naam not in namen_in_netwerk]
 
-st.write('Vaste staf die om een of andere reden niet in netwerk voorkomt:')
-st.write(niet_in_netwerk)
 
-print(len(G.nodes))
+
+
+
+
+
+
+
+'## Onderwijs'
+'''Samenwerking op gebied van onderwijs in de jaren 2019 t/m 2022. Samenwerking is gedefiniëerd als samen in de blokplanningsgroep zitten voor een blok.'''
+
+
+H = gp.create_onderwijs_graph('Data/onderwijs.csv')
+
+if onderwijs_kleur is not 'Geen indeling':
+    gp.kleur_nodes_volgens_kolom(H, onderwijs_kleur, naam_kolom='OnderwijsNaam')
+
+
+onderwijs_net = Network(height='600px', width='1000px', bgcolor='white', font_color='black')
+
+# Take Networkx graph and translate it to a PyVis graph format
+onderwijs_net.from_nx(H)
+
+# Save and read graph as HTML file (on Streamlit Sharing)
+try:
+    path = './tmp'
+    onderwijs_net.save_graph(f'{path}/pyvis_graph.html')
+    HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+
+# Save and read graph as HTML file (locally)
+except:
+    path = './html_files'
+    onderwijs_net.save_graph(f'{path}/pyvis_graph.html')
+    HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+
+# Load HTML file in HTML component for display on Streamlit page
+components.html(HtmlFile.read(), height=600, width=1000)
+
--- graph_prep.py
+++ graph_prep.py
@@ -1,25 +1,37 @@
 import networkx as nx
 import json
 import pandas as pd
+from itertools import combinations
+from collections import Counter
 
-def get_vaste_staf_namelist(excelfile):
+
+HSR_COLORS = ['#001C3D', '#E84E10', '#00A2DB', '#F8B296', '#F8884A', '#005370', '#ABD1FF', '#A1360B']
+
+VASTE_STAF_DF = pd.read_excel('Data/HSR Vaste staf 01-05-2022.XLSX', sheet_name='Sheet1')
+
+def get_vaste_staf_namelist(excelfile, naam_kolom='Volledige naam'):
     namen = pd.read_excel(excelfile, sheet_name='Sheet1')
 
-    return list(namen['Volledige naam'].values)
+    return list(namen[naam_kolom].dropna().values)
 
 VASTE_STAF_NAMEN = get_vaste_staf_namelist('Data/HSR Vaste staf 01-05-2022.XLSX')
-
-
 
 
 def check_if_vaste_staf(naam, vaste_staf_namen=VASTE_STAF_NAMEN):
 
     return naam in vaste_staf_namen
 
+def check_if_vaste_staf_achternaam(achternaam, vaste_staf_namen=VASTE_STAF_NAMEN):
+    vaste_staf_achternamen = [naam.split(' ')[-1] for naam in vaste_staf_namen]
+
+    return achternaam in vaste_staf_achternamen
+
+
 
 
 
 def add_nodes(G, nodejson):
+
     for n in nodejson:
 
         node_name = n['properties']['name']
@@ -32,11 +44,19 @@ def add_nodes(G, nodejson):
 
             G.add_node(n['id'],
             label=node_name) # kleur kan met color='blue'
-
-
+        
     return G
 
 
+def add_nodes_voor_vaste_staf_nog_niet_in_netwerk(G, vaste_staf_namen=VASTE_STAF_NAMEN):
+
+    # Voeg ook mensen toe die wel in vaste staf zitten maar niet in netwerk voorkomen (omdat geen onderzoek of onderwijs)
+    namen_in_netwerk = [G.nodes[id]['label'] for id in G.nodes]
+    niet_in_netwerk = [naam for naam in vaste_staf_namen if naam not in namen_in_netwerk]
+    for staflid in niet_in_netwerk:
+        G.add_node(staflid, label=staflid, shape='square')
+
+    return G
 
 
 def add_edges(G, edgejson):
@@ -63,16 +83,8 @@ def add_edges(G, edgejson):
 
 
 
-# def save_names(G, nodejson):
 
-#     namelist = [name['properties']['name'] for name in nodejson]
-
-#     pd.DataFrame(namelist, columns=['naam']).to_csv('Data/namelist.csv', index=False)
-
-
-
-
-def create_graph(json_filename):
+def create_onderzoek_graph(json_filename):
 
     with open(json_filename, 'r', encoding='utf-8') as infile:
         d = json.load(infile)
@@ -80,6 +92,8 @@ def create_graph(json_filename):
     G = nx.Graph()
 
     G = add_nodes(G, d['nodes'])
+    
+    G = add_nodes_voor_vaste_staf_nog_niet_in_netwerk(G)
 
     G = add_edges(G, d['edges'])
 
@@ -88,4 +102,71 @@ def create_graph(json_filename):
 
 
 
+########## ONDERWIJS GRAPH
+
+
+def create_onderwijs_graph(filename):
+
+    onderwijs = pd.read_csv(filename)
+
+    G = nx.Graph()
+
+    ### NODES
+    stafnamen = onderwijs.Naam.unique()
+    for naam in stafnamen:
+        G.add_node(naam, label=naam, shape='dot')
+
+    G = add_nodes_voor_vaste_staf_nog_niet_in_netwerk(G)
+
+    ### EDGES
+    # Verzamel samenwerkingen binnen blokken
+    alle_samenwerkingen = []
+
+    for unityear in onderwijs.UnitYear.unique():
+
+        # for the edges:
+        # get rows of that unityear
+        # get staff in that unityear
+        samenwerking = onderwijs.loc[onderwijs.UnitYear == unityear, 'Naam'].values
+
+        # get all combinations of 2 of staff in that unityear
+        for c in list(combinations(samenwerking, 2)):
+            alle_samenwerkingen.append(c)
+
+    # alle_samenwerkingen is nu een lijst van 2-tuples.
+    # om de edges gewicht te kunnen geven moeten we tellen hoe vaak elke 2-tuple voorkomt
+    count_alle_samenwerkingen = Counter(alle_samenwerkingen)
     
+    # dan kunnen we de edges toevoegen aan de graph
+    for k, v in count_alle_samenwerkingen.items():
+        G.add_edge(k[0], k[1], weight=v)
+
+    return G
+
+    
+
+def kleur_nodes_volgens_kolom(G, kolom_kleur, naam_kolom='Volledige naam', vaste_staf_df=VASTE_STAF_DF, kleurcodes=HSR_COLORS):
+
+    # unieke waarden in kolom
+    # kleur per unieke waarde
+    kleurdict = {waarde: kleurcodes[i] for i, waarde in enumerate(vaste_staf_df[kolom_kleur].unique())}
+    print(kleurdict)
+
+    # bereid dictionary voor met id: kleur pairs
+    node_kleuren = {}
+    # voor elke node
+    # check unieke waarde en set color attribute
+    for node_id in G.nodes:
+
+        # zoek via naam op welke affiliation iemand heeft
+        node_naam = G.nodes[node_id]['label']
+        try:
+            affiliation = vaste_staf_df.loc[vaste_staf_df[naam_kolom] == node_naam, kolom_kleur].values[0]
+            node_kleuren[node_id] = kleurdict[affiliation]
+        except IndexError:
+            print(node_naam)
+
+    # set dan voor alle nodes de kleur
+    nx.set_node_attributes(G, node_kleuren, name="color")
+
+
